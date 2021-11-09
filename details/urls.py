29dae668122f1c69288b90ea9f4e66a7d8bf8380
mdhvttn <madhav.kothari@tothenew.com>
2021-11-09 18:49:31 +0530

    initial commit

--- details/urls.py
+++ details/urls.py
@@ -2,8 +2,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -15,7 +15,7 @@ urlpatterns = [
     path('<int:pk>/<int:id>/editstudent',views.editStudentDetails,name='editstudent'),
     path('<int:pk>/<int:id>/deletestudent',views.deleteStudentDetails,name='deletestudent'),
     path('logout',views.logoutView,name='logout'),
-]   
+]   + static(settings.STATIC_URL,document_root= settings.STATIC_ROOT)
 
 
 
